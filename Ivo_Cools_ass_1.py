cd2861cb5978537f1e752e4b275dafdefa91e982
IvoC96 <ivo.cools@outlook.com>
2018-11-02 23:09:42 +0100

    Backup 2: containing the calculation (with an error!)

--- Ivo_Cools_ass_1.py
+++ Ivo_Cools_ass_1.py
@@ -1,20 +1,21 @@
-# Waddup kids!
+# author = ivo Cools
 import numpy as np
 import math
 import time as tm
 import matplotlib.pyplot as plt
+
+N = 10
+
 x_min = -0.5
 x_max = 0.5
-N = 20
-
+dx = (x_max - x_min) / (N + 1)
 
 dt = 0.1
-dx = (x_max - x_min)/(N+1)
 
 gamma = 1.4
 rho_1 = 8
 v_1 = 0
-P_1 = 8/gamma
+P_1 = 8 / gamma
 
 rho_2 = 1
 v_2 = 0
@@ -34,6 +35,7 @@ def make_mesh():
 
 
 def initial_initialisation(array, rho_1, v_1, P_1, rho_2, v_2, P_2):
+    #initially initiate the mesh for the first time use. If time permits, here is where I adapt for acoustic difference
     for i in range(N + 2):
         if i < (N + 2) / 2:
             array[0, i] = i
@@ -49,13 +51,15 @@ def initial_initialisation(array, rho_1, v_1, P_1, rho_2, v_2, P_2):
 
 
 def initialisation(array, rho, v, P, i):
+    #function to initialise mesh for every step but i think this one is quite crap
     array[1, i] = rho[i]
     array[2, i] = v[i]
     array[3, i] = P[i]
     return array
 
 
-def comp_diagonal(mesh, i, gamma):  # we also need to calculate the diagonal on each timestep
+def comp_diagonal(mesh, i, gamma):
+    # we also need to calculate the diagonal matrix on each timestep
     rho = mesh[1, i]
     v = mesh[2, i]
     P = mesh[3, i]
@@ -68,6 +72,7 @@ def comp_diagonal(mesh, i, gamma):  # we also need to calculate the diagonal on 
 
 
 def comp_A(mesh, i, gamma):
+    #calculate A  making this bad boy ready to be inverted
     rho = mesh[1, i]
     v = mesh[2, i]
     P = mesh[3, i]
@@ -89,19 +94,59 @@ def create_K(mesh, i):
     K[2, 0] = mesh[3, i]
     return K
 
+def save_to_txt(name,array,header):
+   np.savetxt(name, array, header=header)
 
-def solver(mesh, dt, dx):  # we solve for one time step !
-    #print(mesh)
+
+def solver(mesh, dt, dx):
+    # we solve Euler!
+    # print(mesh)
     U = np.zeros([3, 1])
     rhos = []
     vs = []
     Ps = []
+    rhos_time = []
+    vs_time = []
+    Ps_time = []
     gamma = 1.4
-    for j in np.arange(0.0, 1.0, 1):  # go over the timesteps
+
+    for h in range(N + 2):
+        # i'll have the primitive variables saved to a text-file (each a new text file)
+        # the rhos, vs and Ps will
+        rhos.append(mesh[1, h])
+        vs.append(mesh[2, h])
+        Ps.append(mesh[3, h])
+
+    rhos_2 = rhos[1:N + 1]
+    vs_2 = vs[1:N + 1]
+    Ps_2 = Ps[1:N + 1]
+    rhos_time.append(rhos_2)
+    vs_time.append(vs_2)
+    Ps_time.append(Ps_2)
+
+    for j in np.arange(0.0, 0.3, dt):  # go over the timesteps
+
+        for m in range(N):
+            if m == 0:
+                mesh[1, m] = rhos[m + 1]
+                mesh[2, m] = vs[m + 1]
+                mesh[3, m] = Ps[m + 1]
+            elif m == N + 1:
+                mesh[1, m] = rhos[m - 1]
+                mesh[2, m] = vs[m - 1]
+                mesh[3, m] = Ps[m - 1]
+            else:
+                mesh = initialisation(mesh, rhos, vs, Ps, m)
+
+        # print(mesh)
+        rhos = []
+        vs = []
+        Ps = []
+
         for i in range(1, N + 1):  # go over all points in the mesh
             D = comp_diagonal(mesh, i, gamma)
             A = comp_A(mesh, i, gamma)
-            A_inverse= comp_inverse(A)
+            A_inverse = comp_inverse(A)
             K = create_K(mesh, i)
             """print("in solver wordt de diagonal:")
             print(D)"""
@@ -119,10 +164,10 @@ def solver(mesh, dt, dx):  # we solve for one time step !
             A_inverse_3 = comp_inverse(A_3)
             K_3 = create_K(mesh, i + 1)
             W_3 = np.matmul(A_inverse_3, K_3)
-            print(dt/dx)
+
             for k in range(3):
-                U[k, 0] = W[k, 0] - (dt/dx) * (
-                            (max(D[k, k], 0) * (W[k, 0] - W_2[k, 0])) + (min(D[k, k], 0) * (W_3[k, 0] - W[k, 0])))
+                U[k, 0] = W[k, 0] - (dt / dx) * (
+                        (max(D[k, k], 0) * (W[k, 0] - W_2[k, 0])) + (min(D[k, k], 0) * (W_3[k, 0] - W[k, 0])))
             # beforehand, U was equal to A^-1 * K , but we want K for the pure values
 
             U = np.matmul(A, U)
@@ -133,39 +178,17 @@ def solver(mesh, dt, dx):  # we solve for one time step !
             vs.append(v)
             Ps.append(P)
 
-        # important we reinitalise the mesh after each time step
-
-        for m in range(N):
-            if m == 0:
-                mesh[1, m] = rhos[m + 1]
-                mesh[2, m] = vs[m + 1]
-                mesh[3, m] = Ps[m + 1]
-            elif m == N + 1:
-                mesh[1, m] = rhos[m - 1]
-                mesh[2, m] = vs[m - 1]
-                mesh[3, m] = Ps[m - 1]
-            else:
-                mesh = initialisation(mesh, rhos, vs, Ps, m)
-        print(mesh)
-
-        # U = 1D - vector with U[1] = rho, U[2] = v and U[3] = P
-
-
-"""def initialization (x, w):
-def computetimestep (w, dt):
-def getcmax (w, cmax):
-def integrateintime(x, w, dt):
-def getbc (x, w):
-def get_flux (w, lam, f_upwind):
-def cons_to_diag (w, wdiag):
-def get_kinv (v, csound, Kinv):
-def diag_to_cons (wdiag, w):
-def get_k (v, csound, K):
-def get_eigenval (w, lam):
-def primitive (w, wprim):
-def cv_and_conquer (x, w):
-def chrono (start, start_mess):
-def followup (message):"""
+        rhos_time.append(rhos)
+        vs_time.append(vs)
+        Ps_time.append(Ps)
+        """print(rhos_time)
+        print (vs_time)
+        print (Ps_time)"""
+    print (rhos_time)
+    #every row is a different timestep, to be opened with origin
+    save_to_txt("rho",rhos_time,"rho")
+    save_to_txt("v", vs_time, "v")
+    save_to_txt("P", Ps_time, "P")
 
 A = make_mesh()
 B = initial_initialisation(A, rho_1, v_1, P_1, rho_2, v_2, P_2)
